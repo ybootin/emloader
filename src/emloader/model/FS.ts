5765921d59f998e37939b2c0672ea821739f2ef3
Yohan Boutin <yohan.bout1@gmail.com>
2017-12-07 22:36:00 +0100

    release 0.0.7, fix FS.readFile signature

--- src/emloader/model/FS.ts
+++ src/emloader/model/FS.ts
@@ -3,7 +3,7 @@ declare namespace FS {
   function mkdir(path: string): void
   function mount(fs: FS.IFileSystem, basePath: IPath, path: string): void
   function writeFile(path: string, data: Uint8Array, options: any): void
-  function readFile(path: string, options: any): void
+  function readFile(path: string, options: any): Uint8Array
   interface IPath {
     root: string
   }
